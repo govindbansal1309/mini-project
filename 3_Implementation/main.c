c97a894c8e35f0e6a255acd7a68cc120899f71a8
govindbansal1309 <80391508+govindbansal1309@users.noreply.github.com>
2021-04-14 16:34:08 +0530

    Added main file and makefile

---
+++ 3_Implementation/main.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+#include<ctype.h>
+#include<stdlib.h>
+#include<windows.h>
+#include<time.h>
+#include"headers.h"
+
+void main(void){
+    menu();
+}
