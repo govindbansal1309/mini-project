480bae0b4f1a64efa6321d109a09e26517eaf8ba
govindbansal1309 <80391508+govindbansal1309@users.noreply.github.com>
2021-04-14 16:27:47 +0530

    Added header file

---
+++ 3_Implementation/inc/headers.h
@@ -0,0 +1,80 @@
+#ifndef headers_h
+#define headers_h
+
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+#include<ctype.h>
+#include<stdlib.h>
+#include<windows.h>
+#include<time.h>
+
+#define ENTER 13
+#define BKSP 8
+#define SPACE 32
+#define TAB 9
+
+
+/**
+ * @brief structure for storing product details
+ * 
+ */
+struct item
+{
+	char productname[40],productcomp[40],c;
+	int productid;
+	int price;
+	int Qnt;
+}st;
+
+
+/**
+ * @brief function to give user menu for choosing an operation
+ * 
+ */
+void menu();
+
+
+/**
+ * @brief function to add items in the system
+ * 
+ */
+void add_item();
+
+
+/**
+ * @brief function to display all the items in the system
+ * 
+ */
+void read_item();
+
+
+/**
+ * @brief functionn to search for a particular item
+ * 
+ */
+void search_item();
+
+
+/**
+ * @brief function to edit the details of a particular item
+ * 
+ */
+void edit_item();
+
+
+/**
+ * @brief function to delete a particular item
+ * 
+ */
+void delete_item();
+
+
+/**
+ * @brief function to go on a particular line in files for file handling
+ * 
+ */
+void gotoxy(short, short);
+
+
+#endif
